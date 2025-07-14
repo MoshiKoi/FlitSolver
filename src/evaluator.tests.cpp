966848ce15ff3220262d7ef1bb3f89e44e8af3b7
MoshiKoi <54333972+MoshiKoi@users.noreply.github.com>
2025-07-14 09:43:13 -0700

    Test 1

--- src/evaluator.tests.cpp
+++ src/evaluator.tests.cpp
@@ -40,15 +40,16 @@ TEST_CASE("Best move should try to reach blue", "[evaluator]")
 TEST_CASE("Should capture blue before opponent", "[evaluator]")
 {
 	flit::GameState state{};
-	state.set(4, 5, flit::Cell::Green);
-	state.set(5, 5, flit::Cell::Green);
-	state.set(7, 5, flit::Cell::Blue);
-	state.set(7, 7, flit::Cell::Purple);
-	state.set(7, 8, flit::Cell::Purple);
+	state.set(4, 8, flit::Cell::Green);
+	state.set(5, 8, flit::Cell::Green);
+	state.set(4, 10, flit::Cell::Blue);
+	state.set(8, 8, flit::Cell::Blue);
+	state.set(8, 5, flit::Cell::Purple);
+	state.set(8, 4, flit::Cell::Purple);
 	flit::Solver evaluator{state};
-	auto results = evaluator.solve(flit::Cell::Green, 3);
+	auto results = evaluator.solve(flit::Cell::Green, 5);
 	ASSERT(results.size() > 0);
 	auto [best_move, score] = results[0];
-	ASSERT(best_move.from == flit::from_rc(4, 5));
-	ASSERT(best_move.to == flit::from_rc(6, 5));
+	ASSERT(best_move.from == flit::from_rc(4, 8));
+	ASSERT(best_move.to == flit::from_rc(6, 8));
 }
